435d67cdd1cb332c46570bd26527f5c1d1733b25
Jethro Estangki <estangkij@gmail.com>
2020-07-19 10:44:48 +0800

    Add util folder

---
+++ commands/util/poll.js
@@ -0,0 +1,22 @@
+const { Command } = require('discord.js-commando');
+const pollEmbed = require('../../modules/poll.js');
+
+module.exports = class PollCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'poll',
+			group: 'util',
+			memberName: 'poll',
+			description: 'Make a poll.',
+			format: ''
+		});
+	}
+
+	async run(message, args) {
+		args = args.split(' ');
+		const title = args[0];
+		const options = args.slice(1);
+		pollEmbed(message, title, options)
+			.catch(console.error);
+	}
+}
