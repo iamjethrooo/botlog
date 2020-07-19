7c8728921998ca4e0278beb5ea34ca5eb5adc1fc
Jethro Estangki <estangkij@gmail.com>
2020-07-19 15:43:28 +0800

    Add shoutout for admins

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ client.on('guildCreate', guild => {
 	channel.send('Pa shoutout po kay <@509434286022328340>, hi daw sabi ng asawa nya'); // Morgan
 	channel.send('Pa shoutout po sa Linus ng Baguio, <@321156839020691457> na mahaba rin ang hair'); // Mr. M00
 	channel.send('Pa shoutout po sa server owner nating malupet, <@543589081574015036>'); // Rice
+	channel.send('Pa shoutout po sa mga admin ng server, <@625722729991241756> <@429778312869707776> <@486944028663873536> nice job po mga ser');
 	channel.send('Pa shoutout po kay <@458231661239205888>, tara Raft'); // LLLL
 	channel.send('Pa shoutout po kay <@302076261281300491> who tried to lick a toad once');
 	channel.send('CACAWW! Shoutout po kay <@264666701633683456>, balang araw magiging mod ka rin'); // Crow
