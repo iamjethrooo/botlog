198166f29a7bee3347c165c851e292a8107b9fe2
Jethro Estangki <estangkij@gmail.com>
2020-07-20 10:46:37 +0800

    Remove shoutout messages and add client exports

--- index.js
+++ index.js
@@ -72,25 +72,8 @@ client.on('message', message => {
 	}
 });
 
-client.on('guildCreate', guild => {
-	const channel = client.channels.cache.get('669193383503200266');
-	// channel.send('Pa shoutout po kay <@>');
-	channel.send('Salamat po sa pag invite 🙂');
-	channel.send('Pa shoutout po kay Chief Investigator <@699632157236396082>'); // Gabby
-	channel.send('Pa shoutout po kay <@454079835958935562>, alias Icarus, alias blaze, alias Oni, alias 𝐹𝓇𝑒𝓎𝒶'); // Blaze
-	channel.send('Pa shoutout po kay <@602678848693338134>, musta sa NYC?'); // Gin
-	channel.send('Pa shoutout po kay <@509434286022328340>, hi daw sabi ng asawa nya'); // Morgan
-	channel.send('Pa shoutout po sa Linus ng Baguio, <@321156839020691457> na mahaba rin ang hair'); // Mr. M00
-	channel.send('Pa shoutout po sa server owner nating malupet, <@543589081574015036>'); // Rice
-	channel.send('Pa shoutout po sa mga admin ng server, <@625722729991241756> <@429778312869707776> <@486944028663873536> nice job po mga ser'); // Admins
-	channel.send('Pa shoutout po kay <@458231661239205888>, pro Raft player'); // LLLL
-	channel.send('Pa shoutout po kay <@302076261281300491> who tried to lick a toad once'); // That Guy
-	channel.send('CACAWW! Shoutout po kay <@264666701633683456>, balang araw magiging mod ka rin'); // Crow
-	channel.send('Pa shoutout po kay <@386805742730084353>, sOmeBoDY COme Giiit Heeer, ShE\'S DanCIng LiKE A sStrRIPERR'); // Kaito
-	channel.send('Pa shoutout po kay <@496523098439548929> ang pogi nyo po, sana all'); // Jethro
-	channel.send('Sa mga hindi ko po nashoutout, i-like at share nyo lang po yung facebook page nati para mashoutout ko kayo sa next. 😘');
-});
-
 client.on('error', console.error);
 
 client.login(process.env.DISCORD_TOKEN);
+
+module.exports.client = client;
