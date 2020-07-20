cc06bd6eebde59fc86cdc32347651a4380d0a339
Jethro Estangki <estangkij@gmail.com>
2020-07-20 10:46:48 +0800

    Make burbank command

---
+++ commands/fun/burbank.js
@@ -0,0 +1,24 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class BurbankCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'burbank',
+			group: 'fun',
+			memberName: 'burbank',
+			description: ''
+		});
+	}
+
+	run(message) {
+		const embed = new MessageEmbed()
+			.setColor('#893cbc')
+			.setAuthor('The Burbank')
+			.setThumbnail('https://cdn.discordapp.com/attachments/734048924519759982/734070202118963290/JPEG_20200718_233247.jpg')
+			.setDescription('1 shot Gin\n1 teaspoon Hot Sauce\n1 teaspoon Whole Pepper Corn\n1 teaspoon Bear Brand\n1 teaspoon Turmeric')
+			.setURL('https://discordapp.com/channels/669190303353143306/734048924519759982/734070202500776036')
+			.setFooter('The Burbank');
+		message.say(embed);
+	}
+}
