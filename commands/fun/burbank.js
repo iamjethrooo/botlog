a85556d598287c24a4fe4ed1f7dd701710db9dca
Jethro Estangki <estangkij@gmail.com>
2020-07-20 11:58:31 +0800

    Make author => title

--- commands/fun/burbank.js
+++ commands/fun/burbank.js
@@ -14,7 +14,7 @@ module.exports = class BurbankCommand extends Command {
 	run(message) {
 		const embed = new MessageEmbed()
 			.setColor('#893cbc')
-			.setAuthor('The Burbank')
+			.setTitle('The Burbank')
 			.setThumbnail('https://cdn.discordapp.com/attachments/734048924519759982/734070202118963290/JPEG_20200718_233247.jpg')
 			.setDescription('1 shot Gin\n1 teaspoon Hot Sauce\n1 teaspoon Whole Pepper Corn\n1 teaspoon Bear Brand\n1 teaspoon Turmeric')
 			.setURL('https://discordapp.com/channels/669190303353143306/734048924519759982/734070202500776036')
