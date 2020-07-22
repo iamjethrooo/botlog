8d126e4c6303aa3132b72f4f5a8a1adb4606c401
Jethro Estangki <estangkij@gmail.com>
2020-07-22 16:11:09 +0800

    Make snipe command

---
+++ commands/guild/snipe.js
@@ -0,0 +1,26 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class SnipeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'snipe',
+			group: 'guild',
+			memberName: 'snipe',
+			description: 'Reveal the last deleted message in the channel!'
+		});
+	}
+
+	run(message) {
+		const sniped = this.client.snipes.get(message.channel.id);
+		if (!sniped) {
+			return message.say('There\'s nothing to snipe!');
+		}
+		console.log(sniped.createdAt);
+		const embed = new MessageEmbed()
+			.setAuthor(`${sniped.author.username}#${sniped.author.discriminator}`, sniped.author.displayAvatarURL({ dynamic: true} ))
+			.setDescription(sniped.content)
+			.setTimestamp(sniped.createdAt)
+		return message.say(embed);
+	}
+}
