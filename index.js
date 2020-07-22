3b37a881936d29e838ae7731a8023f5a0a89a079
Jethro Estangki <estangkij@gmail.com>
2020-07-22 16:10:56 +0800

    Remove event listeners

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
+const fs = require('fs');
 
 require('dotenv').config();
 
@@ -28,48 +29,22 @@ client.registry
 	})
 	.registerCommandsIn(path.join(__dirname, 'commands'));
 
+fs.readdir('./events/', (err, files) => {
+	if (err) return console.error(err);
+	files.forEach(file => {
+		const eventFunction = require(`./events/${file}`);
+		if (eventFunction.disabled) return;
 
-client.once('ready', () => {
-	console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-	client.user.setPresence({
-		status: 'online',
-		activity: {
-			name: 'Netflix with Express VPN',
-			type: 'WATCHING',
-			url: 'https://www.twitch.tv/mrmoo_linux'
-		},
-		clientStatus: 'Test'
-	})
-	.catch(console.error);
-});
+		const event = eventFunction.event || file.split('.')[0];
+		const emitter = (typeof eventFunction.emitter === 'string' ? client[eventFunction.emitter] : eventFunction.emitter) || client;
+		const once = eventFunction.once;
 
-const picsOnlyChannels = ['733239970562703411', '669514986548232193', '669520318615912466'];
-client.on('message', message => {
-	const isBot = message.author.bot;
-	// Removes text messages in media-only channels
-	if (picsOnlyChannels.includes(message.channel.id) && !isBot) {
-		
-		const isAdmin = message.member.hasPermission('ADMINISTRATOR');
-		if (!isAdmin) {
-			if (!(message.attachments.size > 0 || message.embeds.length > 0)) {
-				message.delete()
-					.then(message.say(`Don't chat here.`)
-						.then(message => message.delete({ timeout: 5000 }))
-						.catch(console.error)
-						);
-			}
+		try {
+			emitter[once ? 'once' : 'on'](event, (...args) => eventFunction.run(...args));
+		} catch (error) {
+			console.error(error.stack);
 		}
-	}
-
-	// Auto reacts to images posted in #memes in BBC Discord
-	if (message.guild.id == '669190303353143306') {
-		if (message.channel.id == '669355693140213780') {
-			if (message.attachments.size > 0 || message.embeds.length > 0) {
-				message.react(message.guild.emojis.cache.find(emoji => emoji.name == 'DrakeYes'));
-				message.react(message.guild.emojis.cache.find(emoji => emoji.name == 'DrakeNo'));
-			}
-		}
-	}
+	});
 });
 
 client.on('error', console.error);
