49272f6ca73abd9f69d66d30cedb2c7a72932b3f
Jethro Estangki <estangkij@gmail.com>
2020-07-25 19:05:30 +0800

    Fix description

--- commands/fun/pcmr.js
+++ commands/fun/pcmr.js
@@ -8,7 +8,7 @@ module.exports = class ChuckNorrisCommand extends Command {
 			name: 'pcmr',
 			group: 'fun',
 			memberName: 'pcmr',
-			description: 'Spam your chat!',
+			description: 'Get a random top post from the PCMR subreddit.',
 			throttling: {
 				usages: 1,
 				duration: 6,
@@ -17,35 +17,31 @@ module.exports = class ChuckNorrisCommand extends Command {
 	}
 
 	run(message, args) {
-		//if (message.guild.id == '669190303353143306') {
-			//if (message.channel.id == '722700349890363452') {
-				fetch('https://www.reddit.com/r/pcmasterrace/top.json?sort=top', {
-					method: 'GET',
-				})
-					.then(res => res.json())
-					.then(json => {
-						let num;
-						if (args.length > 1) {
-							num = args.split(' ')[0];
-						} else {
-							num = Math.floor(Math.random() * json.data.dist);
-						}
-						const post = json.data.children[num].data;
-						console.log(post.url_overridden_by_dest);
-						console.log(post.subreddit_name_prefixed);
-						const embed = new MessageEmbed()
-							.setTitle(post.title)
-							.setAuthor(`u/${post.author}`)
-							.setURL(`https://www.reddit.com${post.permalink}`)
-							.setImage(post.url_overridden_by_dest)
-							.setFooter(`👍${post.ups} 👎${Math.floor(post.ups - (post.ups * post.upvote_ratio))}`);
-						return message.say(embed);
-					})
-					.catch(err => {
-						message.say('An error occured!');
-						return console.error(err);
-					});
-			//}
-		//}
+		fetch('https://www.reddit.com/r/pcmasterrace/top.json?sort=top', {
+			method: 'GET',
+		})
+			.then(res => res.json())
+			.then(json => {
+				let num;
+				if (args.length > 1) {
+					num = args.split(' ')[0];
+				} else {
+					num = Math.floor(Math.random() * json.data.dist);
+				}
+				const post = json.data.children[num].data;
+				console.log(post.url_overridden_by_dest);
+				console.log(post.subreddit_name_prefixed);
+				const embed = new MessageEmbed()
+					.setTitle(post.title)
+					.setAuthor(`u/${post.author}`)
+					.setURL(`https://www.reddit.com${post.permalink}`)
+					.setImage(post.url_overridden_by_dest)
+					.setFooter(`👍${post.ups} 👎${Math.floor(post.ups - (post.ups * post.upvote_ratio))}`);
+				return message.say(embed);
+			})
+			.catch(err => {
+				message.say('An error occured!');
+				return console.error(err);
+			});
 	}
 };
