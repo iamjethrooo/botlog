71249454521c1f67d484473d7bc596a70b9ef039
Jethro <estangkij@gmail.com>
2021-01-21 14:13:37 +0800

    Redirect people who to #discussion if they chat in restricted channels

--- events/message.js
+++ events/message.js
@@ -1,18 +1,22 @@
-const picsOnlyChannels = ['733239970562703411', '669514986548232193', '669520318615912466', '735401124135174185', '669514957938753558', '671669574961201157', '732928099938992128', '800543054221541376'];
+const picsOnlyChannels = ['669514957938753558', '800543054221541376', '669514986548232193', '671669574961201157', '801677818760003594'];
+const index = require('../index.js')
+const client = index.client;
+
 module.exports = {
 	run: message => {
 		if (message.guild === null) return;
 		const isBot = message.author.bot;
 		// Removes text messages in media-only channels
 		if (picsOnlyChannels.includes(message.channel.id) && !isBot) {
-			
 			const isAdmin = message.member.hasPermission('ADMINISTRATOR');
 			if (!isAdmin) {
 				if (!(message.attachments.size > 0 || message.embeds.length > 0)) {
-					//console.log(message);
+					const discussionChannel = client.channels.cache.get('735804432985620521');
+					console.log(message);
 					message.delete()
 						.then(message.say(`Don't chat here, please use the <#735804432985620521> channel above.`)
 							.then(message => message.delete({ timeout: 5000 }))
+							.then(discussionChannel.send(`**Message removed in:** <#${message.channel.id}>\n**Message sent by:** <@${message.author.id}>\n**Message content:** ${message.content}`))
 							.catch(console.error)
 							)
 						.catch(console.error);
