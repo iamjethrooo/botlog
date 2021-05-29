420bcdfd4fa5ea6b6899396776069c0e1b6658c8
Jethro <estangkij@gmail.com>
2021-05-29 20:11:09 +0800

    Cut down embed if length exceeds 800

--- commands/util/userinfo.js
+++ commands/util/userinfo.js
@@ -27,7 +27,7 @@ async run(message, args) {
 		var count = 0;
 		for (var i = 0; i < roles.length; i++) {
 			count += roles[i].length + 1;
-			if (count < 900) {
+			if (count < 800) {
 				max = i;
 			} else {
 				cut = true;
