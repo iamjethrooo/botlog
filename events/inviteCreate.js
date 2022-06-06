d39ae381760666922023884588adb77e09e270b6
Jethro <estangkij@gmail.com>
2022-06-06 22:17:24 +0800

    Delete files from discord.js@v12

--- events/inviteCreate.js
+++
@@ -1,9 +0,0 @@
-const index = require('../index.js');
-const client = index.client;
-
-module.exports = {
-	run: async invite => {
-    // Add new invite to the cache when a new one is created
-    client.invites.set(invite.guild.id, await invite.guild.fetchInvites());
-	}
-}
