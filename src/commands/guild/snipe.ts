cf455a477506b41ec363d5193940f66c4ece3c18
Jethro <estangkij@gmail.com>
2022-06-29 14:12:33 +0800

    Allow selecting number of message to snipe

--- src/commands/guild/snipe.ts
+++ src/commands/guild/snipe.ts
@@ -3,9 +3,10 @@ import {
   ApplicationCommandRegistry,
   Command,
   CommandOptions,
+  Args,
   container
 } from '@sapphire/framework';
-import { CommandInteraction, MessageEmbed, Message, GuildMember } from 'discord.js';
+import { CommandInteraction, MessageEmbed, Message, GuildMember, TextChannel } from 'discord.js';
 
 @ApplyOptions<CommandOptions>({
   name: 'snipe',
@@ -39,28 +40,51 @@ export class SnipeCommand extends Command {
     return await interaction.reply({ ephemeral: true, content: 'You don\'t have the permissions to run this command!' });
   }
 
-  public override async messageRun(message: Message) {
+  public override async messageRun(message: Message, args: Args) {
     if (!message.guild) {
       return await message.reply(`You can't use this command in a DM!`);
     }
+
+
     const { client } = container;
-    const sniped = client.snipes.get(message.channel.id);
+
 
     if (message.member!.permissions.has('ADMINISTRATOR') || message.member!.roles.cache.find(r => r.name === 'Sniper' || r.name === 'Enforcer' || r.name === 'Ruby (Lvl. 75+)' || r.name === 'Emerald (Lvl. 60+)' || r.name === 'Sapphire (Lvl. 45+)' || r.name === 'Steel (Lvl. 30+)' || r.name === 'Obsidian (Lvl. 15+)')) {
+      const sniped = client.snipes.get(message.channel.id);
       if (!sniped) {
         return await message.reply('There\'s nothing to snipe!').then(message => setTimeout(() => message.delete(), 15000)).then(message.delete);
       }
-      let content = `**${sniped[0].author}**: ${sniped[0].content}`;
-      if (sniped.length > 1) {
-        content = `**${sniped[1].author}**: ${sniped[1].content}\n` + content;
+      let num = await args.pick('integer').catch(() => -1);
+
+      if (num <= 0) {
+        let content = `**${sniped[0].author}**: ${sniped[0].content}`;
+        if (sniped.length > 1) {
+          content = `**${sniped[1].author}**: ${sniped[1].content}\n` + content;
+        }
+        if (sniped.length > 2) {
+          content = `**${sniped[2].author}**: ${sniped[2].content}\n` + content;
+        }
+        const embed = new MessageEmbed()
+          .setDescription(content)
+          .setColor(message.member!.displayHexColor!)
+        return await message.reply({ embeds: [embed] });
+      } else {
+        num--;
+        if (!sniped[num]) {
+          return await message.reply('There\'s nothing to snipe!').then(message => setTimeout(() => message.delete(), 15000)).then(message.delete);
+        }
+        const singleSnipe = sniped[num];
+        const embed = new MessageEmbed()
+          .setAuthor(`${singleSnipe.author.username}#${singleSnipe.author.discriminator}`, singleSnipe.author.displayAvatarURL({ dynamic: true }))
+          .setDescription(singleSnipe.content)
+          .setTimestamp(singleSnipe.createdAt)
+          .setColor(message.member!.displayHexColor)
+        if (singleSnipe.channel instanceof TextChannel) {
+          embed.setFooter("#".concat(singleSnipe.channel.name));
+        }
+        return await message.reply({ embeds: [embed] });
+
       }
-      if (sniped.length > 2) {
-        content = `**${sniped[2].author}**: ${sniped[2].content}\n` + content;
-      }
-      const embed = new MessageEmbed()
-        .setDescription(content)
-        .setColor(message.member!.displayHexColor!)
-      return await message.reply({ embeds: [embed] });
     }
     return await message.reply('You don\'t have the permissions to run this command!').then(message => setTimeout(() => message.delete(), 15000));
   }
