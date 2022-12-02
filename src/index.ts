c3495a406ec5ed758216e4cc5e1433682819f829
Jethro <estangkij@gmail.com>
2022-12-02 14:11:05 +0800

    Remove music player

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,19 @@
-import { load } from '@lavaclient/spotify';
 import {
   ApplicationCommandRegistries,
   RegisterBehavior
 } from '@sapphire/framework';
 import type { NewsChannel, TextChannel, ThreadChannel } from 'discord.js';
-import { Player } from 'lavaclient';
-import { Queue } from './lib/utils/queue/Queue';
-import type { Song } from './lib/utils/queue/Song';
 import { ExtendedClient } from './structures/ExtendedClient';
 require('dotenv').config();
-
-load({
-  client: {
-    id: process.env.SPOTIFY_CLIENT_ID!,
-    secret: process.env.SPOTIFY_CLIENT_SECRET!
-  },
-  autoResolveYoutubeTracks: true
-});
 
 const client = new ExtendedClient();
 
 client.on('ready', async () => {
-  client.music.connect(client.user!.id);
   client.user ?.setStatus('online');
 });
 
 
 export type MessageChannel = TextChannel | ThreadChannel | NewsChannel | null;
-
-declare module 'lavaclient' {
-  interface Player {
-    readonly queue: Queue;
-    [_queue]: Queue;
-    nightcore: boolean;
-    vaporwave: boolean;
-    karaoke: boolean;
-    bassboost: boolean;
-  }
-
-  interface ClusterEvents {
-    nodeQueueCreate: (node: ClusterNode, queue: Queue) => void;
-    nodeQueueFinish: (node: ClusterNode, queue: Queue) => void;
-    nodeTrackStart: (node: ClusterNode, queue: Queue, song: Song) => void;
-    nodeTrackEnd: (node: ClusterNode, queue: Queue, song: Song) => void;
-  }
-
-  interface NodeEvents {
-    queueCreate: (queue: Queue) => void;
-    queueFinish: (queue: Queue) => void;
-    trackStart: (queue: Queue, song: Song) => void;
-    trackEnd: (queue: Queue, song: Song) => void;
-  }
-}
-
-const _queue: unique symbol = Symbol.for('Player#queue');
-Reflect.defineProperty(Player.prototype, 'queue', {
-  get(this: Player) {
-    return (this[_queue] ??= new Queue(this));
-  }
-});
 
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(
   RegisterBehavior.Overwrite
--- src/structures/ExtendedClient.ts
+++ src/structures/ExtendedClient.ts
@@ -1,16 +1,8 @@
 import { SapphireClient } from '@sapphire/framework';
 import { Intents, Message } from 'discord.js';
-import { Node } from 'lavaclient';
-import { embedButtons } from '../lib/utils/music/ButtonHandler';
-import { NowPlayingEmbed } from './../lib/utils/music/NowPlayingEmbed';
-import { manageStageChannel } from './../lib/utils/music/channelHandler';
-import { inactivityTime } from '../lib/utils/music/handleOptions';
 require('dotenv').config();
 
 export class ExtendedClient extends SapphireClient {
-  readonly music: Node;
-  playerEmbeds: { [key: string]: string };
-  leaveTimers: { [key: string]: NodeJS.Timer };
   snipes: Map<string, Message[]>;
   editsnipes: Map<string, Message>;
 
@@ -28,78 +20,11 @@ export class ExtendedClient extends SapphireClient {
 
     this.snipes = new Map<string, Message[]>();
     this.editsnipes = new Map<string, Message>;
-
-    this.music = new Node({
-      sendGatewayPayload: (id, payload) =>
-        this.guilds.cache.get(id) ?.shard ?.send(payload),
-      connection: {
-        host: process.env.LAVA_HOST!,
-        password: process.env.LAVA_PASS!,
-        port: parseInt(process.env.LAVA_PORT!),
-        secure: process.env.LAVA_SECURE! == 'true'
-      }
-    });
-
-    // on VOICE_SERVER_UPDATE
-    this.ws.on('VOICE_SERVER_UPDATE', data => {
-      this.music.handleVoiceUpdate(data);
-    });
-    // on VOICE_STATE_UPDATE
-    this.ws.on('VOICE_STATE_UPDATE', data => {
-      this.music.handleVoiceUpdate(data);
-    });
-
-    this.playerEmbeds = {};
-    this.leaveTimers = {};
-
-    // on queueFinish
-    this.music.on('queueFinish', queue => {
-      queue.player.stop();
-
-      this.leaveTimers[queue.player.guildId] = setTimeout(() => {
-        queue.channel!.send(':zzz: Leaving due to inactivity');
-        queue.player.disconnect();
-        queue.player.node.destroyPlayer(queue.player.guildId);
-      }, inactivityTime());
-      delete this.playerEmbeds[queue.player.guildId];
-    });
-
-    // on trackStart
-    this.music.on('trackStart', async (queue, song) => {
-      if (this.leaveTimers[queue.player.guildId]) {
-        clearTimeout(this.leaveTimers[queue.player.guildId]!);
-      }
-
-      const NowPlaying = new NowPlayingEmbed(
-        song,
-        0,
-        queue.current!.length as number,
-        queue.player.volume,
-        queue.tracks!,
-        queue.last!,
-        false
-      );
-
-      await embedButtons(NowPlaying.NowPlayingEmbed(), queue, song);
-
-      const voiceChannel = this.voice.client.channels.cache.get(
-        queue.player.channelId!
-      );
-
-      // Stage Channels
-      if (voiceChannel ?.type === 'GUILD_STAGE_VOICE') {
-        const botUser = voiceChannel ?.members.get(this.application ?.id!);
-        await manageStageChannel(voiceChannel, botUser!, queue);
-      }
-    });
   }
 }
 
 declare module '@sapphire/framework' {
   interface SapphireClient {
-    readonly music: Node;
-    playerEmbeds: { [key: string]: string };
-    leaveTimers: { [key: string]: NodeJS.Timer };
     snipes: Map<string, Message[]>;
     editsnipes: Map<string, Message>;
   }
