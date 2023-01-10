8b413f507c1ea061a893953da2bc9e8a1ce3136a
Jethro <estangkij@gmail.com>
2023-01-10 20:15:22 +0800

    Use embeds instead of plain text

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,15 +1,19 @@
 // @ts-nocheck
-import { ApplyOptions } from '@sapphire/decorators';
-import {
-  Listener,
-  ListenerOptions,
-  container
-} from '@sapphire/framework';
-import { Message, Util } from 'discord.js';
-const picsOnlyChannels = ['669514957938753558', '800543054221541376', '669514986548232193', '671669574961201157', '801677818760003594', '978228788561346571', '1044996825754644670'];
+import { ApplyOptions } from "@sapphire/decorators";
+import { Listener, ListenerOptions, container } from "@sapphire/framework";
+import { Message, MessageEmbed, Util } from "discord.js";
+const picsOnlyChannels = [
+  "669514957938753558",
+  "800543054221541376",
+  "669514986548232193",
+  "671669574961201157",
+  "801677818760003594",
+  "978228788561346571",
+  "1044996825754644670",
+];
 
 @ApplyOptions<ListenerOptions>({
-  event: 'messageCreate'
+  event: "messageCreate",
 })
 export class MessageListener extends Listener {
   public override async run(message: Message): Promise<void> {
@@ -20,13 +24,14 @@ export class MessageListener extends Listener {
 
     // Removes text messages in media-only channels
     if (picsOnlyChannels.includes(message.channel.id)) {
-      const isAdmin = message.member!.permissions.has('ADMINISTRATOR');
+      const isAdmin = message.member!.permissions.has("ADMINISTRATOR");
       if (!isAdmin) {
         // For discussions category
         if (!(message.attachments.size > 0 || message.embeds.length > 0)) {
           // Store values related to the message
           // discussionChannel: '735804432985620521'
-          const discussionChannel = client.channels.cache.get('735804432985620521');
+          const discussionChannel =
+            client.channels.cache.get("901646890817445948");
           const guildId = message.guild.id;
           const channelId = message.channel.id;
           const authorId = message.author.id;
@@ -34,20 +39,40 @@ export class MessageListener extends Listener {
           let hasReplied = false;
 
           if (message.reference) hasReplied = true;
-          let referenceId;
-          if (hasReplied) referenceId = message.reference!.messageId;
-          message.delete().then(message.channel.send(`Don't chat here, please use the <#735804432985620521> channel above.`).then(message => setTimeout(() => message.delete(), 5000)));
-          if (content.includes('@everyone') || content.includes('@here')) {
+          message
+            .delete()
+            .then(
+              message.channel
+                .send(
+                  `Don't chat here, please use the <#735804432985620521> channel above.`
+                )
+                .then((message) => setTimeout(() => message.delete(), 5000))
+            );
+          if (content.includes("@everyone") || content.includes("@here")) {
             content = Util.removeMentions(content);
           }
 
+          let embed = new MessageEmbed()
+            .setAuthor(
+              `${message.author.username}#${message.author.discriminator}`,
+              message.author.displayAvatarURL({ dynamic: true })
+            )
+            .addField("Message content:", content)
+            .setTimestamp(message.createdAt)
+            .setColor(message.member!.displayHexColor)
+            .setFooter("#".concat(message.channel.name));
+          discussionChannel!.send(`<@${authorId}>`);
           // If author replied to a message
           if (hasReplied) {
-            return discussionChannel!.send(`**Message removed in:** <#${channelId}>\n**Message sent by:** <@${authorId}>\n**Message content:** ${content}\n**Message replied to:** https://discord.com/channels/${guildId}/${channelId}/${message.reference.messageID}`)
+            let referenceId = message.reference!.messageId!;
+            embed.addField(
+              "Replied to:",
+              `[Click to jump to message.](https://discord.com/channels/${guildId}/${channelId}/${referenceId})`
+            );
+            return discussionChannel!.send({ embeds: [embed] });
           }
-          else {
-            return discussionChannel!.send(`**Message removed in:** <#${channelId}>\n**Message sent by:** <@${authorId}>\n**Message content:** ${content}`)
-          }
+
+          return discussionChannel!.send({ embeds: [embed] });
         }
       }
     }
