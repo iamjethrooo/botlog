40f787e13e153d7fef8a3c41d9c5f130754729cc
Jethro <estangkij@gmail.com>
2023-01-15 04:49:26 +0800

    Add help command and change logo text

--- packages/dashboard/src/components/Logo/Logo.tsx
+++ packages/dashboard/src/components/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
     <h1 className="font-bold text-transparent w-max text-6xl bg-clip-text bg-gradient-to-r from-red-600 to-amber-500">
-      <span>Master Bot</span>
+      <span>BBC Bot</span>
     </h1>
   );
 };
