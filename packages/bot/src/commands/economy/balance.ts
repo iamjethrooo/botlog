952a1bff8230b21c32646347a6905150b02ab090
Jethro <estangkij@gmail.com>
2023-01-17 04:40:28 +0800

    Add bal command

---
+++ packages/bot/src/commands/economy/balance.ts
@@ -0,0 +1,52 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import {
+  ApplicationCommandRegistry,
+  Command,
+  CommandOptions,
+} from "@sapphire/framework";
+import { CommandInteraction, Message, MessageEmbed } from "discord.js";
+import { trpcNode } from "../../trpc";
+
+@ApplyOptions<CommandOptions>({
+  name: "balance",
+  aliases: ["bal"],
+  description: "Replies with Pong!",
+})
+export class BalanceCommand extends Command {
+  public override async chatInputRun(interaction: CommandInteraction) {}
+
+  public override async messageRun(message: Message) {
+    console.log(message);
+    try {
+      let user = await trpcNode.user.getUserById.query({
+        id: message.author.id,
+      });
+
+      const embed = new MessageEmbed()
+        .setAuthor(
+          `${message.author.username}#${message.author.discriminator}`,
+          message.author.displayAvatarURL({ dynamic: true })
+        )
+        .addFields(
+          { name: "Cash:", value: '<:baguiobenguetchat:854546677897625600>' + String(user!.user!.coins), inline: true },
+          { name: "Bank:", value: '<:baguiobenguetchat:854546677897625600>' + String(0), inline: true },
+          { name: "Total:", value: '<:baguiobenguetchat:854546677897625600>' + String(user!.user!.coins), inline: true }
+        )
+        .setTimestamp(message.createdAt)
+        .setColor(message.member!.displayHexColor);
+      return await message.reply({ embeds: [embed] });
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  }
+
+  public override registerApplicationCommands(
+    registery: ApplicationCommandRegistry
+  ): void {
+    registery.registerChatInputCommand({
+      name: this.name,
+      description: this.description,
+    });
+  }
+}
