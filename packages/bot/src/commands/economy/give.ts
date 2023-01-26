0642d48a14008fafba32cd12be639a748f7a4b39
Jethro <estangkij@gmail.com>
2023-01-27 03:56:39 +0800

    Add bot response when running the command

--- packages/bot/src/commands/economy/give.ts
+++ packages/bot/src/commands/economy/give.ts
@@ -6,7 +6,7 @@ import {
   Args,
   container,
 } from "@sapphire/framework";
-import { CommandInteraction, Message } from "discord.js";
+import { CommandInteraction, Message, MessageEmbed } from "discord.js";
 import { trpcNode } from "../../trpc";
 
 @ApplyOptions<CommandOptions>({
@@ -23,6 +23,7 @@ export class GiveCommand extends Command {
 
     let amount = await args.pick("integer").catch(() => 0);
 
+    // Give cash to a user
     if (message.mentions.users.size == 1) {
       console.log(message.mentions.users!.first()!.id);
       let id = message.mentions.users!.first()!.id;
@@ -47,45 +48,81 @@ export class GiveCommand extends Command {
             cash: amount,
           });
         }
+
+        const embed = new MessageEmbed()
+          .setAuthor(
+            `${message.author.username}#${message.author.discriminator}`,
+            message.author.displayAvatarURL({ dynamic: true })
+          )
+          .setDescription(
+            `Gave <@${id}> <:baguiobenguetchat:854546677897625600>${String(
+              amount
+            )}.`
+          )
+          .setTimestamp(message.createdAt)
+          .setColor(message.member!.displayHexColor);
+        return await message.reply({ embeds: [embed] });
       } catch (error) {
         console.log(error);
         return;
       }
-    } else if (message.mentions.roles) {
+    }
+    // Give cash to a role
+    else if (message.mentions.roles) {
       let id = message.mentions.roles.first()!.id;
 
       const { client } = container;
       const guild = client.guilds.cache.get(message.guildId!);
-      guild!.members.fetch().then(members => members.forEach(async member => {
-        if (member.roles.cache.some(role => role.id == id)) {
-          try {
-            // Check if user exists in database
-            let user = await trpcNode.user.getUserById.query({
-              id: member.user.id,
-            });
-    
-            if (user.user == null) {
-              await trpcNode.user.create.mutate({
+      try {
+        guild!.members.fetch().then((members) =>
+          members.forEach(async (member) => {
+            if (member.roles.cache.some((role) => role.id == id)) {
+              //
+
+              // Check if user exists in database
+              let user = await trpcNode.user.getUserById.query({
                 id: member.user.id,
-                name: message.author.username,
               });
-              await trpcNode.user.addCash.mutate({
-                id: member.user.id,
-                cash: Number(process.env.STARTING_CASH) + amount,
-              });
-            } else {
-              await trpcNode.user.addCash.mutate({
-                id: member.user.id,
-                cash: amount,
-              });
+
+              if (user.user == null) {
+                await trpcNode.user.create.mutate({
+                  id: member.user.id,
+                  name: message.author.username,
+                });
+                await trpcNode.user.addCash.mutate({
+                  id: member.user.id,
+                  cash: Number(process.env.STARTING_CASH) + amount,
+                });
+              } else {
+                await trpcNode.user.addCash.mutate({
+                  id: member.user.id,
+                  cash: amount,
+                });
+              }
             }
-          } catch (error) {
-            console.log(error);
-            return;
-          }
-        }
-      }));
+          })
+        );
+
+        const embed = new MessageEmbed()
+          .setAuthor(
+            `${message.author.username}#${message.author.discriminator}`,
+            message.author.displayAvatarURL({ dynamic: true })
+          )
+          .setDescription(
+            `Gave <@&${id}> <:baguiobenguetchat:854546677897625600>${String(
+              amount
+            )}.`
+          )
+          .setTimestamp(message.createdAt)
+          .setColor(message.member!.displayHexColor);
+        return await message.reply({ embeds: [embed] });
+      } catch (error) {
+        console.log(error);
+        return;
+      }
     }
+
+    return;
   }
 
   public override registerApplicationCommands(
