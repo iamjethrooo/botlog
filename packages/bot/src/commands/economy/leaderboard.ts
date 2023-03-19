8cc35912d93492ae5f074bbd612c8bbb42c2d11d
Jethro <estangkij@gmail.com>
2023-03-19 12:35:01 +0800

    Implement slash command

--- packages/bot/src/commands/economy/leaderboard.ts
+++ packages/bot/src/commands/economy/leaderboard.ts
@@ -15,7 +15,46 @@ import { trpcNode } from "../../trpc";
   preconditions: ["inBotChannel"],
 })
 export class LeaderboardCommand extends Command {
-  public override async chatInputRun(interaction: CommandInteraction) {}
+  public override async chatInputRun(interaction: CommandInteraction) {
+  
+    let leaderboard = await trpcNode.user.getLeaderboard.query();
+    let leaderboardFormatted: String[] = [];
+    let rank = 0;
+    let index = 1;
+
+    await interaction.guild!.members.fetch();
+
+    leaderboard.leaderboard.forEach((user) => {
+      let member = interaction.guild!.members.cache.get(user!.discordId!);
+      let isMod = member ? member!.permissions.has("ADMINISTRATOR") : false;
+
+      if (isMod) {
+        return;
+      }
+      
+      if (interaction.user.id == user.discordId) {
+        rank = index;
+      }
+      leaderboardFormatted.push(
+        `**${index}.** <@${user.discordId}> ・ ${process.env.COIN_EMOJI}${user.cash}`
+      );
+      index++;
+    });
+    const baseEmbed = new MessageEmbed()
+      .setColor("#FF0000")
+      .setAuthor({
+        name: interaction!.guild!.name,
+        iconURL: interaction!.guild!.iconURL()!,
+      })
+      .setFooter(`Your leaderboard rank: ${rank == 0 ? "N/A" : rank}`);
+    new PaginatedFieldMessageEmbed()
+      .setTitleField("Leaderboard")
+      .setTemplate(baseEmbed)
+      .setItems(leaderboardFormatted)
+      .setItemsPerPage(10)
+      .make()
+      .run(interaction);
+  }
 
   public override async messageRun(message: Message) {
     let leaderboard = await trpcNode.user.getLeaderboard.query();
