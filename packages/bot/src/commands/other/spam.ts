a72f66bed0a39f038e679905d4fd12357e8cb0b9
Jethro <estangkij@gmail.com>
2023-04-06 08:27:20 +0800

    Update splitMessage command

--- packages/bot/src/commands/other/spam.ts
+++ packages/bot/src/commands/other/spam.ts
@@ -14,7 +14,10 @@ import fetch from "node-fetch";
 })
 export class SpamCommand extends Command {
   public override async chatInputRun(interaction: CommandInteraction) {
-    if (interaction.channelId != "682838969179832423") {
+    if (
+      interaction.guildId == "669190303353143306" &&
+      interaction.channelId != "682838969179832423"
+    ) {
       return await interaction.reply({
         ephemeral: true,
         content: "You can't use that command here!",
@@ -26,7 +29,9 @@ export class SpamCommand extends Command {
       .then((res) => res.json())
       .then((json) => {
         const rand = Math.floor(Math.random() * json.data.dist);
-        const split = SpamCommand.splitMessage(json.data.children[rand].data.selftext);
+        const split = SpamCommand.splitMessage(
+          json.data.children[rand].data.selftext
+        );
         for (let s of split) {
           interaction.channel!.send(s);
         }
@@ -40,7 +45,10 @@ export class SpamCommand extends Command {
   }
 
   public override async messageRun(message: Message) {
-    if (message.channel.id != "682838969179832423") {
+    if (
+      message.guildId == "669190303353143306" &&
+      message.channel.id != "682838969179832423"
+    ) {
       return;
     }
     fetch("https://www.reddit.com/r/copypasta/new.json?sort=top", {
@@ -49,7 +57,9 @@ export class SpamCommand extends Command {
       .then((res) => res.json())
       .then((json) => {
         const rand = Math.floor(Math.random() * json.data.dist);
-        const split = SpamCommand.splitMessage(json.data.children[rand].data.selftext);
+        const split = SpamCommand.splitMessage(
+          json.data.children[rand].data.selftext
+        );
         for (let s of split) {
           message.channel.send(s);
         }
@@ -62,7 +72,28 @@ export class SpamCommand extends Command {
   }
 
   public static splitMessage(str: String) {
-    return str.match(/[\s\S]{1,3}/g) || [];
+    const MAX_LENGTH = 2000;
+    const words = str.split(" ");
+    const result: string[] = [];
+    let currentLine = "";
+
+    // Iterate through each word in the input string
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i]; // Current word being processed
+
+      if (currentLine.length + word.length + 1 <= MAX_LENGTH) {
+        currentLine += (currentLine ? " " : "") + word;
+      } else {
+        result.push(currentLine);
+        currentLine = word;
+      }
+    }
+
+    if (currentLine) {
+      result.push(currentLine);
+    }
+
+    return result;
   }
 
   public override registerApplicationCommands(
